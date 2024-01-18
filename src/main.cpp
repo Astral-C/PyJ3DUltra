6f21b98708b685947ec1966ee44c585d16afa0e0
LagoLunatic <LagoLunatic@users.noreply.github.com>
2024-01-17 21:49:52 -0500

    Add full support for all animation types

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 #include <J3D/Data/J3DModelData.hpp>
 #include <J3D/Material/J3DUniformBufferObject.hpp>
 #include <J3D/Animation/J3DColorAnimationInstance.hpp>
+#include <J3D/Animation/J3DTexIndexAnimationInstance.hpp>
+#include <J3D/Animation/J3DTexMatrixAnimationInstance.hpp>
+#include <J3D/Animation/J3DJointAnimationInstance.hpp>
+#include <J3D/Animation/J3DJointFullAnimationInstance.hpp>
+#include <J3D/Animation/J3DVisibilityAnimationInstance.hpp>
 #include <J3D/Animation/J3DAnimationLoader.hpp>
 #include <J3D/Rendering/J3DRendering.hpp>
 #include <J3D/Rendering/J3DLight.hpp>
@@ -159,6 +164,251 @@ std::shared_ptr<J3DAnimation::J3DColorAnimationInstance> LoadBrk(std::string pat
     return animInstance;
 }
 
+void attachBtp(std::shared_ptr<J3DModelInstance> instance, py::bytes data){
+    if(!init) return;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DTexIndexAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DTexIndexAnimationInstance>(animLoaded);
+
+    instance->SetTexIndexAnimation(animInstance);
+}
+
+void attachBtp(std::shared_ptr<J3DModelInstance> instance, std::string path){
+    if(!init) return;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DTexIndexAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DTexIndexAnimationInstance>(path);
+
+    instance->SetTexIndexAnimation(animInstance);
+}
+
+void attachBtp(std::shared_ptr<J3DModelInstance> instance, std::shared_ptr<J3DAnimation::J3DTexIndexAnimationInstance> anim){
+    if(!init) return;
+    instance->SetTexIndexAnimation(anim);
+}
+
+std::shared_ptr<J3DAnimation::J3DTexIndexAnimationInstance> LoadBtp(py::bytes data){
+    if(!init) return nullptr;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DTexIndexAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DTexIndexAnimationInstance>(animLoaded);
+
+    return animInstance;
+}
+
+std::shared_ptr<J3DAnimation::J3DTexIndexAnimationInstance> LoadBtp(std::string path){
+    if(!init) return nullptr;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DTexIndexAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DTexIndexAnimationInstance>(path);
+
+    return animInstance;
+}
+
+void attachBtk(std::shared_ptr<J3DModelInstance> instance, py::bytes data){
+    if(!init) return;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DTexMatrixAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DTexMatrixAnimationInstance>(animLoaded);
+
+    instance->SetTexMatrixAnimation(animInstance);
+}
+
+void attachBtk(std::shared_ptr<J3DModelInstance> instance, std::string path){
+    if(!init) return;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DTexMatrixAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DTexMatrixAnimationInstance>(path);
+
+    instance->SetTexMatrixAnimation(animInstance);
+}
+
+void attachBtk(std::shared_ptr<J3DModelInstance> instance, std::shared_ptr<J3DAnimation::J3DTexMatrixAnimationInstance> anim){
+    if(!init) return;
+    instance->SetTexMatrixAnimation(anim);
+}
+
+std::shared_ptr<J3DAnimation::J3DTexMatrixAnimationInstance> LoadBtk(py::bytes data){
+    if(!init) return nullptr;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DTexMatrixAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DTexMatrixAnimationInstance>(animLoaded);
+
+    return animInstance;
+}
+
+std::shared_ptr<J3DAnimation::J3DTexMatrixAnimationInstance> LoadBtk(std::string path){
+    if(!init) return nullptr;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DTexMatrixAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DTexMatrixAnimationInstance>(path);
+
+    return animInstance;
+}
+
+void attachBck(std::shared_ptr<J3DModelInstance> instance, py::bytes data){
+    if(!init) return;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DJointAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DJointAnimationInstance>(animLoaded);
+
+    instance->SetJointAnimation(animInstance);
+}
+
+void attachBck(std::shared_ptr<J3DModelInstance> instance, std::string path){
+    if(!init) return;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DJointAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DJointAnimationInstance>(path);
+
+    instance->SetJointAnimation(animInstance);
+}
+
+void attachBck(std::shared_ptr<J3DModelInstance> instance, std::shared_ptr<J3DAnimation::J3DJointAnimationInstance> anim){
+    if(!init) return;
+    instance->SetJointAnimation(anim);
+}
+
+void attachBca(std::shared_ptr<J3DModelInstance> instance, py::bytes data){
+    if(!init) return;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DJointFullAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DJointFullAnimationInstance>(animLoaded);
+
+    instance->SetJointFullAnimation(animInstance);
+}
+
+void attachBca(std::shared_ptr<J3DModelInstance> instance, std::string path){
+    if(!init) return;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DJointFullAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DJointFullAnimationInstance>(path);
+
+    instance->SetJointFullAnimation(animInstance);
+}
+
+void attachBca(std::shared_ptr<J3DModelInstance> instance, std::shared_ptr<J3DAnimation::J3DJointFullAnimationInstance> anim){
+    if(!init) return;
+    instance->SetJointFullAnimation(anim);
+}
+
+std::shared_ptr<J3DAnimation::J3DJointFullAnimationInstance> LoadBca(py::bytes data){
+    if(!init) return nullptr;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DJointFullAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DJointFullAnimationInstance>(animLoaded);
+
+    return animInstance;
+}
+
+std::shared_ptr<J3DAnimation::J3DJointFullAnimationInstance> LoadBca(std::string path){
+    if(!init) return nullptr;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DJointFullAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DJointFullAnimationInstance>(path);
+
+    return animInstance;
+}
+
+void attachBva(std::shared_ptr<J3DModelInstance> instance, py::bytes data){
+    if(!init) return;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DVisibilityAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DVisibilityAnimationInstance>(animLoaded);
+
+    instance->SetVisibilityAnimation(animInstance);
+}
+
+void attachBva(std::shared_ptr<J3DModelInstance> instance, std::string path){
+    if(!init) return;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DVisibilityAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DVisibilityAnimationInstance>(path);
+
+    instance->SetVisibilityAnimation(animInstance);
+}
+
+void attachBva(std::shared_ptr<J3DModelInstance> instance, std::shared_ptr<J3DAnimation::J3DVisibilityAnimationInstance> anim){
+    if(!init) return;
+    instance->SetVisibilityAnimation(anim);
+}
+
+std::shared_ptr<J3DAnimation::J3DVisibilityAnimationInstance> LoadBva(py::bytes data){
+    if(!init) return nullptr;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DVisibilityAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DVisibilityAnimationInstance>(animLoaded);
+
+    return animInstance;
+}
+
+std::shared_ptr<J3DAnimation::J3DVisibilityAnimationInstance> LoadBva(std::string path){
+    if(!init) return nullptr;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DVisibilityAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DVisibilityAnimationInstance>(path);
+
+    return animInstance;
+}
+
+std::shared_ptr<J3DAnimation::J3DJointAnimationInstance> LoadBck(py::bytes data){
+    if(!init) return nullptr;
+
+    py::buffer_info dataInfo(py::buffer(data).request());
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    std::shared_ptr<J3DAnimation::J3DAnimationInstance> animLoaded = Loader.LoadAnimation(dataInfo.ptr, dataInfo.size);
+    std::shared_ptr<J3DAnimation::J3DJointAnimationInstance> animInstance = std::dynamic_pointer_cast<J3DAnimation::J3DJointAnimationInstance>(animLoaded);
+
+    return animInstance;
+}
+
+std::shared_ptr<J3DAnimation::J3DJointAnimationInstance> LoadBck(std::string path){
+    if(!init) return nullptr;
+
+    J3DAnimation::J3DAnimationLoader Loader;
+    
+    std::shared_ptr<J3DAnimation::J3DJointAnimationInstance> animInstance = Loader.LoadAnimation<J3DAnimation::J3DJointAnimationInstance>(path);
+
+    return animInstance;
+}
+
 void setLight(std::shared_ptr<J3DModelInstance> instance, J3DLight light, int lightIdx){
     instance->SetLight(light, lightIdx);
 }
@@ -218,10 +468,29 @@ PYBIND11_MODULE(J3DUltra, m) {
         .def(py::init<>())
         .def("createInstance", &J3DModelData::CreateInstance);
 
-    py::class_<J3DAnimation::J3DColorAnimationInstance, std::shared_ptr<J3DAnimation::J3DColorAnimationInstance>>(m, "J3DColorAnimation")
-        .def(py::init<>())
+    py::class_<J3DAnimation::J3DAnimationInstance, std::shared_ptr<J3DAnimation::J3DAnimationInstance>>(m, "J3DAnimation")
         .def("setFrame", &J3DAnimation::J3DAnimationInstance::SetFrame)
-        .def("getFrame", &J3DAnimation::J3DAnimationInstance::GetFrame);
+        .def("getFrame", &J3DAnimation::J3DAnimationInstance::GetFrame)
+        .def("setPaused", &J3DAnimation::J3DAnimationInstance::SetPaused)
+        .def("tick", &J3DAnimation::J3DAnimationInstance::Tick);
+
+    py::class_<J3DAnimation::J3DColorAnimationInstance, std::shared_ptr<J3DAnimation::J3DColorAnimationInstance>, J3DAnimation::J3DAnimationInstance>(m, "J3DColorAnimation")
+        .def(py::init<>());
+
+    py::class_<J3DAnimation::J3DTexIndexAnimationInstance, std::shared_ptr<J3DAnimation::J3DTexIndexAnimationInstance>, J3DAnimation::J3DAnimationInstance>(m, "J3DTexIndexAnimation")
+        .def(py::init<>());
+
+    py::class_<J3DAnimation::J3DTexMatrixAnimationInstance, std::shared_ptr<J3DAnimation::J3DTexMatrixAnimationInstance>, J3DAnimation::J3DAnimationInstance>(m, "J3DTexMatrixAnimation")
+        .def(py::init<>());
+
+    py::class_<J3DAnimation::J3DJointAnimationInstance, std::shared_ptr<J3DAnimation::J3DJointAnimationInstance>, J3DAnimation::J3DAnimationInstance>(m, "J3DJointAnimation")
+        .def(py::init<>());
+
+    py::class_<J3DAnimation::J3DJointFullAnimationInstance, std::shared_ptr<J3DAnimation::J3DJointFullAnimationInstance>, J3DAnimation::J3DAnimationInstance>(m, "J3DJointFullAnimation")
+        .def(py::init<>());
+
+    py::class_<J3DAnimation::J3DVisibilityAnimationInstance, std::shared_ptr<J3DAnimation::J3DVisibilityAnimationInstance>, J3DAnimation::J3DAnimationInstance>(m, "J3DVisibilityAnimation")
+        .def(py::init<>());
 
     py::class_<J3DModelInstance, std::shared_ptr<J3DModelInstance>>(m, "J3DModelInstance")
         .def(py::init<std::shared_ptr<J3DModelData>>())
@@ -234,18 +503,48 @@ PYBIND11_MODULE(J3DUltra, m) {
         .def("attachBrk", py::overload_cast<std::shared_ptr<J3DModelInstance>, py::bytes>(&attachBrk), py::kw_only(), py::arg("data"))
         .def("attachBrk", py::overload_cast<std::shared_ptr<J3DModelInstance>, std::string>(&attachBrk), py::kw_only(), py::arg("path"))
         .def("attachBrk", &J3DModelInstance::SetRegisterColorAnimation, py::kw_only(), py::arg("anim"))
-        .def("getBrk", &J3DModelInstance::GetRegisterColorAnimation);
-        
-
+        .def("getBrk", &J3DModelInstance::GetRegisterColorAnimation)
+        .def("attachBtp", py::overload_cast<std::shared_ptr<J3DModelInstance>, py::bytes>(&attachBtp), py::kw_only(), py::arg("data"))
+        .def("attachBtp", py::overload_cast<std::shared_ptr<J3DModelInstance>, std::string>(&attachBtp), py::kw_only(), py::arg("path"))
+        .def("attachBtp", &J3DModelInstance::SetTexIndexAnimation, py::kw_only(), py::arg("anim"))
+        .def("getBtp", &J3DModelInstance::GetTexIndexAnimation)
+        .def("attachBtk", py::overload_cast<std::shared_ptr<J3DModelInstance>, py::bytes>(&attachBtk), py::kw_only(), py::arg("data"))
+        .def("attachBtk", py::overload_cast<std::shared_ptr<J3DModelInstance>, std::string>(&attachBtk), py::kw_only(), py::arg("path"))
+        .def("attachBtk", &J3DModelInstance::SetTexMatrixAnimation, py::kw_only(), py::arg("anim"))
+        .def("getBtk", &J3DModelInstance::GetTexMatrixAnimation)
+        .def("attachBck", py::overload_cast<std::shared_ptr<J3DModelInstance>, py::bytes>(&attachBck), py::kw_only(), py::arg("data"))
+        .def("attachBck", py::overload_cast<std::shared_ptr<J3DModelInstance>, std::string>(&attachBck), py::kw_only(), py::arg("path"))
+        .def("attachBck", &J3DModelInstance::SetJointAnimation, py::kw_only(), py::arg("anim"))
+        .def("getBck", &J3DModelInstance::GetJointAnimation)
+        .def("attachBca", py::overload_cast<std::shared_ptr<J3DModelInstance>, py::bytes>(&attachBca), py::kw_only(), py::arg("data"))
+        .def("attachBca", py::overload_cast<std::shared_ptr<J3DModelInstance>, std::string>(&attachBca), py::kw_only(), py::arg("path"))
+        .def("attachBca", &J3DModelInstance::SetJointFullAnimation, py::kw_only(), py::arg("anim"))
+        .def("getBca", &J3DModelInstance::GetJointFullAnimation)
+        .def("attachBva", py::overload_cast<std::shared_ptr<J3DModelInstance>, py::bytes>(&attachBva), py::kw_only(), py::arg("data"))
+        .def("attachBva", py::overload_cast<std::shared_ptr<J3DModelInstance>, std::string>(&attachBva), py::kw_only(), py::arg("path"))
+        .def("attachBva", &J3DModelInstance::SetVisibilityAnimation, py::kw_only(), py::arg("anim"))
+        .def("getBva", &J3DModelInstance::GetVisibilityAnimation)
+        ;
+    
     m.def("loadModel", py::overload_cast<std::string>(&LoadJ3DModel), "Load BMD/BDL from filepath", py::kw_only(), py::arg("path"));
     m.def("loadModel", py::overload_cast<py::bytes>(&LoadJ3DModel), "Load BMD/BDL from bytes object", py::kw_only(), py::arg("data"));
     
-    m.def("loadBrk", py::overload_cast<std::string>(&LoadBrk), "Load BMD/BDL from filepath", py::kw_only(), py::arg("path"));
-    m.def("loadBrk", py::overload_cast<py::bytes>(&LoadBrk), "Load BMD/BDL from bytes object", py::kw_only(), py::arg("data"));
-
+    m.def("loadBrk", py::overload_cast<std::string>(&LoadBrk), "Load BRK from filepath", py::kw_only(), py::arg("path"));
+    m.def("loadBrk", py::overload_cast<py::bytes>(&LoadBrk), "Load BRK from bytes object", py::kw_only(), py::arg("data"));
+    m.def("loadBtp", py::overload_cast<std::string>(&LoadBtp), "Load BTP from filepath", py::kw_only(), py::arg("path"));
+    m.def("loadBtp", py::overload_cast<py::bytes>(&LoadBtp), "Load BTP from bytes object", py::kw_only(), py::arg("data"));
+    m.def("loadBtk", py::overload_cast<std::string>(&LoadBtk), "Load BTK from filepath", py::kw_only(), py::arg("path"));
+    m.def("loadBtk", py::overload_cast<py::bytes>(&LoadBtk), "Load BTK from bytes object", py::kw_only(), py::arg("data"));
+    m.def("loadBck", py::overload_cast<std::string>(&LoadBck), "Load BCK from filepath", py::kw_only(), py::arg("path"));
+    m.def("loadBck", py::overload_cast<py::bytes>(&LoadBck), "Load BCK from bytes object", py::kw_only(), py::arg("data"));
+    m.def("loadBca", py::overload_cast<std::string>(&LoadBca), "Load BCA from filepath", py::kw_only(), py::arg("path"));
+    m.def("loadBca", py::overload_cast<py::bytes>(&LoadBca), "Load BCA from bytes object", py::kw_only(), py::arg("data"));
+    m.def("loadBva", py::overload_cast<std::string>(&LoadBva), "Load BVA from filepath", py::kw_only(), py::arg("path"));
+    m.def("loadBva", py::overload_cast<py::bytes>(&LoadBva), "Load BVA from bytes object", py::kw_only(), py::arg("data"));
+    
     m.def("init", &InitJ3DUltra, "Setup J3DUltra for Model Loading and Rendering");
     m.def("cleanup", &CleanupJ3DUltra, "Cleanup J3DUltra Library");
     m.def("setCamera", &SetCamera, "Set Projection and View Matrices to render with");
-
+    
     m.def("render", &RenderScene, "Execute all pending model renders");
 }
